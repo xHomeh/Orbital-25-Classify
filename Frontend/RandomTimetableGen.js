b288764bb96922f0a3fade632a9375a464c17a87
xHomeh <xhomeh@gmail.com>
2025-05-06 16:16:27 +0800

    Update RandomTimetableGen.js
    Made more functions instead of just doing it all inside 1 function, and added some comments.
    
    Successfully extracted by module and class types.
    
    Need to make it key value pairs though ?

--- Frontend/RandomTimetableGen.js
+++ Frontend/RandomTimetableGen.js
@@ -1,3 +1,4 @@
+
 async function fetchModuleData(courseCodes) {
     const urlTemplate = "https://api.nusmods.com/v2/2023-2024/modules/";
   
@@ -19,17 +20,64 @@ async function fetchModuleData(courseCodes) {
       return [];
     }
   }
-  
 
-function generateRandom(apiDataArray, semester) {
-  const results = apiDataArray.map(modData => modData.semesterData)
-  const semData = results.map(yearData => yearData[semester - 1]);
-  const timetableData = semData.map(dict => dict.timetable);
-  return timetableData;
+function getDistinct(value, index, array) {
+  return array.indexOf(value) === index;
+}
+
+// Use this by doing getTimetableData(apiData, semester).map(getClassTypes)
+// function getClassTypes(courseArrayOfDicts) {
+//   return courseArrayOfDicts.map(dict => dict.lessonType).filter(getDistinct);
+// }
+
+// Get the relevant semester data which contains class timings from the API data 
+function getAcadYearData(apiDataArray){
+  return apiDataArray.map(modData => modData.semesterData);
+}
+
+// Filter out the sem we want ie sem 1 or 2
+function getSemData(apiDataArray, semester) {
+  return getAcadYearData(apiDataArray).map(yearData => yearData[semester - 1]);
+}
+
+// Get the list of class timings 
+function getTimetableData(apiDataArray, semester) {
+  return getSemData(apiDataArray, semester).map(dict => dict.timetable);
+}
+
+// Get the type of lessons there is in the mod, ie tutorial lecture etc. 
+function getClassTypes(apiDataArray, semester) {
+  return getTimetableData(apiDataArray, semester).map(arrOfDicts => arrOfDicts.map(dict => dict.lessonType).filter(getDistinct));
 }
 
 
+/*
+get the lesson types from each mod, ie recitation, tutorial, lecture. 
+Map each lesson type to the possible timings, then sort by least lessons so less likely to clash (for efficiency)
+Shuffle the array of timings for randomisation 
+Use recursive backtracking to get the first possible combination
 
+Possible issues: If there is no valid combination it will run thru every combination which will likely take quite long
+*/
+
+function generateRandom(apiDataArray, semester) {
+  const timetableData = getTimetableData(apiDataArray, semester);
+  const classTypes = getClassTypes(apiDataArray, semester);
+  
+  const classTypeWithTimings = [];
+
+  for (let i = 0; i < classTypes.length; i++) {
+    const classTimesForModuleAtIndexI = timetableData[i];
+    const temp = [];
+    for (let j = 0; j < classTypes[i].length; j++) {
+      temp[j] = classTimesForModuleAtIndexI.filter(dict => dict.lessonType == classTypes[i][j]); 
+    }
+
+    classTypeWithTimings[i] = temp;
+  }
+
+  return classTypeWithTimings;
+}
 
 
 
@@ -39,4 +87,10 @@ function generateRandom(apiDataArray, semester) {
   //   console.log("Fetched Modules:", data);
   // });
 
-  fetchModuleData(modules).then(data => generateRandom(data, 1)).then(data => console.log(data));
+  // get class types
+  fetchModuleData(modules).then(data => getClassTypes(data, 1)).then(x => console.log(x));
+
+  fetchModuleData(modules).then(data => generateRandom(data, 1)).then(x => console.log(x));
+
+  // fetchModuleData(modules).then(x => getTimetableData(x, 1)).then(data => console.log(data));
+

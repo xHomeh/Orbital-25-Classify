f27d4d30794ad24b0fe3f2f7f9e2effb462e2e46
xHomeh <xhomeh@gmail.com>
2025-05-26 16:06:04 +0800

    changes to test

--- Frontend/src/pages/SettingsPage.js
+++ Frontend/src/pages/SettingsPage.js
@@ -9,11 +9,16 @@ function SettingsPage() {
                 <Link to="/login" className="flex w-[1/10] h-full justify-start text-orange-600 text-xl block text-right px-3 py-2 absolute top-0 right-0 hover:text-white">Login</Link>
             </div>
             <hr className="border-gray-400" />
-            <div className="flex flex-col gap-2 text-gray-400 mt-3 px-3">
-                <Link to="/" className="hover:text-orange-600 hover:underline">Timetable</Link>
-                <Link to="/friends" className="hover:text-orange-600 hover:underline">Friends</Link>
-                <Link to="/settings" className="text-orange-600 hover:underline">Settings</Link>
+
+            <div className="flex gap-2">
+                <div className="flex flex-col gap-2 text-gray-400 mt-3">
+                    <Link to="/" className="hover:text-orange-600 hover:underline px-4">Timetable</Link>
+                    <Link to="/friends" className="hover:text-orange-600 hover:underline px-4">Friends</Link>
+                    <Link to="/settings" className="text-orange-600 hover:underline px-4">Settings</Link>
+                </div>
+                <div className="border-2 px-3 mt-3 text-gray-400">Settings</div>
             </div>
+
             <h1 className="text-gray-300 mt-4 mx-4">WORK IN PROGRESS!</h1>
         </div>
     );

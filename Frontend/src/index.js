489dfda1bb4fdaf84d4ad54081130a4545afe5dc
xHomeh <xhomeh@gmail.com>
2025-06-22 16:30:00 +0800

    axios add import

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter, RouterProvider, Routes, Route, createBrowserRouter} from 'react-router-dom';
+import Axios from 'axios'
 
 import HomePage from './pages/HomePage';
 import Login from './pages/Login';

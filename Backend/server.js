958c82649d740781d42e6bcb1d3c8f3a868d0fb1
xHomeh <xhomeh@gmail.com>
2025-06-25 00:00:27 +0800

    change mysql to mysql2

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 console.log("DB Host:", process.env.DB_HOST);
 
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 
@@ -11,7 +11,7 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT || 3306
 });
 
 db.connect((err) => {
